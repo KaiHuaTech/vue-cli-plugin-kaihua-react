66c20a45270765f5cc3824fc04ba311f2b47b56c
soluteli <soluteli@163.com>
2021-03-08 18:30:07 +0800

    chore: add packages

--- generator/index.js
+++ generator/index.js
@@ -10,12 +10,21 @@ module.exports = (...args) => {
   // 生成 react app file
   api.render('./gen-react-app/template')
   
+  // 删除项目初始文件 https://github.com/vuejs/vue-cli/blob/7f56846a2e54ee5b748232e8b5ae2411a147416d/packages/@vue/cli/lib/Generator.js#L173
+  Object.keys(api.generator.files).forEach(p => {
+    const _path = p.replace(/\\/g, '/')
+    if (_path.endsWith('.vue') || _path.endsWith('src/assets/logo.png')) {
+      Reflect.deleteProperty(api.generator.files, _path)
+    }
+  })
+  
   api.extendPackage({
     dependencies: {
       "react": ">16",
       "react-dom": ">16",
     }
   })
+
 
   if (options.useEslint) {
     api.genEslint(...args)
