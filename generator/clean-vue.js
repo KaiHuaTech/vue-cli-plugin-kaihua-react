0a1058c370c4c2cc2e8ef26334092ddc4cbf0db0
soluteli <soluteli@163.com>
2021-03-08 17:04:01 +0800

    fix: 调整 generator 为同步函数

--- generator/clean-vue.js
+++ generator/clean-vue.js
@@ -1,10 +1,10 @@
 const { loadModule } = require('@vue/cli-shared-utils')
-module.exports = async (api, options) => {
+module.exports = (api, options) => {
   // 1. 删除原项目 vue 项目 src、public 目录文件
   const fs = loadModule('fs-extra', api.generator.context)
-  await fs.emptyDir(api.resolve('./src'))
-  await fs.emptyDir(api.resolve('./public'))
+  fs.emptyDirSync(api.resolve('./src'))
+  fs.emptyDirSync(api.resolve('./public'))
 
   // 2. 删除 package.json vue 项目依赖
-  await api.generator.pm.remove('vue') 
+  api.generator.pm.remove('vue') 
 }
--- generator/index.js
+++ generator/index.js
@@ -3,19 +3,19 @@ const { loadModule } = require('@vue/cli-shared-utils')
 const cleanVue = require('./clean-vue')
 const genEslint = require('./gen-eslint')
 const genBabel = require('./gen-babel')
-module.exports = async (...args) => {
+module.exports = (...args) => {
   const [api, options] = args
-  await cleanVue(api, options)
+  cleanVue(api, options)
 
+  // 生成 react app file
+  api.render('./gen-react-app/template')
+  
   api.extendPackage({
     dependencies: {
       "react": ">16",
       "react-dom": ">16",
     }
   })
-
-  // 生成 react app file
-  api.render('./gen-react-app/template')
 
   if (options.useEslint) {
     api.genEslint(...args)
